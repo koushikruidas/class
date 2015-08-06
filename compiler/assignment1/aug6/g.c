b7a3756a95ea4345022f9c84c9be5c423bc75003
koushikruidas <koushikruidas@gmail.com>
2015-08-06 11:32:02 +0530

    day4

---
+++ compiler/assignment1/aug6/g.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int  main(){
+	int a, sum = 0;
+	printff("enter a no .\n");
+	scanff("%d",&a);
+	sum = 29 + a;
+	printf("the sumation of this no to 29 is : %d\n",sum);
+}
---
+++ compiler/assignment1/aug6/out.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int  main(){
+	int a, sum = 0;
+	writef("enter a no .\n");
+	readf("%d",&a);
+	sum = 29 + a;
+	write("the sumation of this no to 29 is : %d\n",sum);
+}
